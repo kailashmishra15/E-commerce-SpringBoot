897a646df283093773509dd40efa821a67751087
kailash mishra <103932027+kailashmishra15@users.noreply.github.com>
2026-03-17 19:25:18 +0530

    security fix

--- src/main/java/com/mp/auth/config/JwtAuthenticationFilter.java
+++ src/main/java/com/mp/auth/config/JwtAuthenticationFilter.java
@@ -33,7 +33,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-        
+        String path = request.getServletPath();
+        if (path.startsWith("/api/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
